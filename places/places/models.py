1d3d14e6ab0a645f14ded57c8e435c684488c6f2
marianadiaz179 <78369609+marianadiaz179@users.noreply.github.com>
2022-11-30 18:54:59 -0500

    Update models.py

--- places/places/models.py
+++ places/places/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Place(models.Model):
     name = models.CharField(max_length=50)
-    #Measurements 
 
     def __str__(self):
         return '%s %s' % (self.value, self.unit)
